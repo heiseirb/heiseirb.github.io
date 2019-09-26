b7749f2a61d1174dfabb9391c749dc8911c3cf6d
uryu_myao <[myao.jpn@gmail.com]>
2019-09-26 17:37:30 +0900

    第二弾コーディング追加

--- assets/js/kaigi01.js
+++ assets/js/kaigi01.js
@@ -1,3 +1,39 @@
 "use strict";
 
-console.log("hi");
+// const tl = new TimelineMax({
+//     paused: true
+// });
+
+// smooth scroll to div from header
+window.smoothScroll = function (target) {
+    var scrollContainer = target;
+    do {
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do {
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function (c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function () { scroll(c, a, b, i); }, 20);
+    }
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+
+
+// back to top 
+let timeOut;
+function scrollToTop() {
+    if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0) {
+        window.scrollBy(0, -50);
+        timeOut = setTimeout('scrollToTop()', 5);
+    }
+    else clearTimeout(timeOut);
+}
